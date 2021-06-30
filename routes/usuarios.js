b559186f2849ea83f5f2c0e53b81727d59348972
Andres Martinez <andresmartinez@andresmartinez.local>
2021-06-30 18:41:38 -0300

    Metodo put en usuarios funcionando

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -14,8 +14,21 @@ ruta.post('/', (req, res) => {
         .then(user => {
             res.json({ valor: user });
         })
-        .catch(err => {res.status(400).json({error: err})})
-})
+        .catch(err => { res.status(400).json({ error: err }) })
+});
+
+ruta.put('/:id', (req, res) => {
+    let resultado = actualizarUsuario(req.params.id, req.body);
+    resultado.then(valor => {
+        res.json({
+            valor: valor
+        })
+    }).catch(err => {
+        res.status(400).json({
+            error: err
+        })
+    })
+});
 
 async function crearUsuario(body) {
     let usuario = new Usuario({
@@ -24,6 +37,18 @@ async function crearUsuario(body) {
         password: body.password
     });
     return await usuario.save();
+}
+
+async function actualizarUsuario(id, body) {
+    const usuario = await Usuario.findByIdAndUpdate(id, {
+        $set: {
+            nombre: body.nombre,
+            password: body.password,
+            email: body.email
+        }
+    }, {new: true})
+    console.log(usuario);
+    return usuario;
 }
 
 module.exports = ruta;
